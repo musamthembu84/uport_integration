477b3081ee71884f786cbb3f5dbd1af45f34f332
musamthembu84@gmail.com <Streetwise22@22>
2020-02-27 21:12:04 +0200

    updated project whic is the final version

---
+++ contracts/Election.sol
@@ -0,0 +1,43 @@
+pragma solidity >=0.4.21 <0.7.0;
+
+contract Election {
+
+    struct Candidate {
+        uint id;
+        string name;
+        uint voteCount;
+    }
+
+    mapping (uint => Candidate) public candidates;
+    mapping (address => bool) public voters; //store accounts that have voted
+    uint public candidatesCount;
+
+    constructor () public {
+        addCandidates("EFFSC");
+        addCandidates("Sasco");
+        addCandidates("Daso");
+    }
+
+    function addCandidates(string memory _name) private{
+        candidatesCount++;
+        candidates[candidatesCount] = Candidate(candidatesCount,_name,0);
+    }
+
+    event votedEvent(
+        uint indexed _candidateId
+    );
+
+    function vote (uint _candidateId) public {
+        require(!voters[msg.sender]); //requires that person has not voted before;
+        require(_candidateId>0 && _candidateId<=candidatesCount); //requires a valid candidate
+
+        voters[msg.sender]=true; //recording vote
+
+        candidates[_candidateId].voteCount ++; //update candidatesCount
+
+        emit votedEvent(_candidateId); //prove of vote
+
+    }
+
+
+}
---
+++ src/layouts/home/Home.js
@@ -0,0 +1,106 @@
+import React, { Component } from 'react'
+import '../../css/bootstrap.min.css'
+import App from "../../App";
+
+import Election from '../../../build/contracts/Election'
+import Web3 from "web3";
+class Home extends Component {
+
+  componentWillMount() {
+      this.loadBlockchainData()
+  }
+  async loadBlockchainData() {
+      const web3 = new Web3(Web3.givenProvider || "http://localhost:8545")
+      const accounts = await web3.eth.getAccounts();
+      this.setState({ account: accounts[0] });
+
+      const networkID = await web3.eth.net.getId();
+      const networkData = Election.networks[networkID];
+
+      const election = web3.eth.Contract(Election.abi,networkData.address);
+      this.setState({election});
+
+      const candidateCount = await election.methods.candidatesCount().call();
+      this.setState({candidateCount})
+
+      //load all candidate
+    for (let i =1; i<=candidateCount;i++){
+         const allCandidates = await  election.methods.candidates(i).call();
+         this.setState({
+           candidates:[...this.state.candidates, allCandidates]
+         })
+
+    }
+
+    console.log(this.state.candidates);
+  }
+
+  constructor(props){
+      super(props);
+      this.state = {
+        account: '',
+        candidateCount:0,
+        candidates:[]
+      }
+  }
+
+
+  render() {
+    return(
+
+
+       <div className="container">
+          <div className="row">
+            <div className="col-lg-12">
+              <h1 className="text-center">Election Results</h1>
+              <hr/>
+              <br/>
+              <div id="loader">
+                <p className="text-center">Loading...</p>
+              </div>
+
+              <div id="content">
+                <table className="table">
+                  <thead>
+                    <tr>
+                      <th scope="col">#</th>
+                      <th scope="col">Name </th>
+                      <th scope="col">Votes</th>
+                    </tr>
+                  </thead>
+                  <tbody id="candidatesResults">
+                  {this.state.candidates.map((candidate,key)=> {
+                    return (
+                        <tr key={key}>
+                          <th scope="row">{candidate.id.toString()}</th>
+                          <td>{candidate.name}</td>
+                          <td>{candidate.voteCount.toString()}</td>
+                        </tr>
+                    )
+                  })}
+                  </tbody>
+                </table>
+                <hr/>
+
+                <form onSubmit="">
+                  <div className="form-group">
+                    <label htmlFor="candidatesSelect">Select Candidate</label>
+                    <select className="form-control" id="candidatesSelect">
+                    </select>
+                  </div>
+                  <button type="submit" className="btn btn-primary">Vote</button>
+                  <hr/>
+                </form>
+
+                <p id="accountAddress" className="text-center">Account {this.state.account} </p>
+              </div>
+
+
+            </div>
+          </div>
+        </div>
+    )
+  }
+}
+
+export default Home
